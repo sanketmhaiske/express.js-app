d97556cb94f6828e411ad5561c8eb29edb1c9c2f
sanketmhaiske <sanketmhaiske2017@gmail.com>
2023-10-04 11:51:26 +0530

    Fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use("/", product);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
