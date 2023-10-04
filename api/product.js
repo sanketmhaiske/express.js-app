be1ca1e0e90438ace50c6b5443c262cbe0be22a8
sanketmhaiske <sanketmhaiske2017@gmail.com>
2023-10-04 11:48:54 +0530

    Bug Fixing

--- api/product.js
+++ api/product.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-/**
- * GET product list.
- *
- * @return product list | empty.
- */
-router.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
-  }
+router.get("/", (req, res) => {
+    return res.status(200).send('Running on Vercel')
 });
 
 module.exports = router;
